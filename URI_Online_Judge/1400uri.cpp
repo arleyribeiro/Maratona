7b4ab50b9b46f2b03e23cc8ab7e17b73064eded1
arleyribeiro <arley.sribeiro@gmail.com>
2016-04-21 14:32:22 -0300

    Simulation

---
+++ URI_Online_Judge/1400uri.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool has_sete(int n){
+    int aux;
+    while(n){
+        aux = n % 10;
+        if(aux == 7)
+            return true;
+        n /= 10;
+    } 
+    return false;
+}
+
+int main() {
+	int n,m,k,x,c;
+
+	while(scanf("%d %d %d",&n,&m,&k), m+n+k) {
+		x=c=0;
+		while(k) {
+			for(int i=1;i<n;i++) {
+				++c;
+				if(i==m){
+					//printf("I : %d %d\n", i, c);
+					if(c%7==0)
+						k--;
+					else if(has_sete(c))
+						k--;
+				}
+				if(k==0)
+				break;
+			}
+
+			if(k==0)
+				break;
+
+			for(int i=n;i>1;i--) {
+				++c;
+				if(i==m){
+					//printf("i : %d %d\n", i, c);
+					if(c%7==0)
+						k--;
+					else if(has_sete(c))
+						k--;
+				}
+				if(k==0)
+				break;			
+			}
+			/*if(c>100000 && k){
+				c=-1;
+				break;	
+			}*/
+		}
+
+		printf("%d\n", c);
+
+	}
+	
+	
+	return 0;
+}
